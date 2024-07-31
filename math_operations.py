64d983ed5f6a40ec895fac1553512da945cad5d4
Ana Trisovic <ana.trisovic@gmail.com>
2024-07-31 11:10:21 -0500

    first commit

---
+++ math_operations.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(
+    the_first_number_to_multiply_this_is_a_long_name_for_a_variable,
+    the_second_number_to_multiply,
+):
+    """This funcation takes two numbers a and b and multiplies them."""
+    return (
+        the_first_number_to_multiply_this_is_a_long_name_for_a_variable
+        * the_second_number_to_multiply
+    )
+
+
+print(add(10, 5))
